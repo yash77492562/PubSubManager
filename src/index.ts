510eb08dd00b4b7bc44920c7a1bd5bf6a92e9304
yash77492562 <yashyadavpro@gmail.com>
2024-11-07 08:56:49 +0530

    update index.ts and pubSubManager.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { startLogger } from "./logger";
 import { GameManager } from "./store";
+import { PubSubManager } from "./PubSubMananger";
+
 
 startLogger()
 
 setInterval(()=>{
     GameManager.getInstance().addGame(Math.random().toString())
+    PubSubManager.getInstance().userSubscribe(Math.random().toString(), "APPL");
 },5000)
+
